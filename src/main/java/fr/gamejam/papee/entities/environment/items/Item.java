c0b51cdcb6cf2aa40ed77d3ddbe59efc6162abee
Maxime Coupas <maxime.coupas@avanade.com>
2018-12-15 15:05:49 +0100

    Implement Items

--- src/main/java/fr/gamejam/papee/entities/environment/items/Item.java
+++ src/main/java/fr/gamejam/papee/entities/environment/items/Item.java
@@ -3,9 +3,8 @@ package fr.gamejam.papee.entities.environment.items;
 import fr.gamejam.papee.entities.environment.EnvironmentObject;
 
 public abstract class Item extends EnvironmentObject {
+    
     public Item(int ID, float x, float y, ItemType itemType, String name) {
         super(ID, x, y, 32, 32, false, itemType, name);
-
-
     }
 }
--- src/main/java/fr/gamejam/papee/entities/environment/items/ItemBeer.java
+++ src/main/java/fr/gamejam/papee/entities/environment/items/ItemBeer.java
@@ -1,7 +1,6 @@
 package fr.gamejam.papee.entities.environment.items;
 
 import fr.gamejam.papee.entities.PaPee;
-import fr.gamejam.papee.entities.environment.EnvironmentObject;
 
 public class ItemBeer extends Item {
 
