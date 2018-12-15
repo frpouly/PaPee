0683fefbee9101375446368c21a9a9e07d3662b2
Louis Dutel <ldutel@itnetwork.fr>
2018-12-15 18:24:58 +0100

    add obstacle

--- src/main/java/fr/gamejam/papee/entities/environment/obstacles/Wardrobe.java
+++ src/main/java/fr/gamejam/papee/entities/environment/obstacles/Wardrobe.java
@@ -3,20 +3,12 @@ package fr.gamejam.papee.entities.environment.obstacles;
 import fr.gamejam.papee.entities.PaPee;
 import fr.gamejam.papee.entities.environment.items.ItemType;
 
-public class Poop extends Obstacle {
+public class Wardrobe extends Obstacle {
 
-    public Poop(int ID, float x, float y) {
-        super(ID, x, y, ItemType.MALUS, "poop");
-    }
-
-
+    public Wardrobe(int ID, float x, float y) { super(ID, x, y, ItemType.MALUS, "wardrobe"); }
+    
     @Override
     public void effect(PaPee papee) {
 
-    }
-
-    @Override
-    public boolean isRigid(){
-        return false;
     }
 }
