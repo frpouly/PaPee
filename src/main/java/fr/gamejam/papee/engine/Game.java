784f1939945a89326752d2d6f4f66d14be2216e2
ArmenAri <armenaristakes@gmail.com>
2018-12-15 15:44:58 +0100

    Game.java

--- src/main/java/fr/gamejam/papee/engine/Game.java
+++ src/main/java/fr/gamejam/papee/engine/Game.java
@@ -6,36 +6,19 @@ import fr.gamejam.papee.engine.game.GGame;
 import fr.gamejam.papee.engine.guis.GGui;
 import fr.gamejam.papee.engine.objects.GObject;
 import fr.gamejam.papee.engine.utils.GDefines;
+import fr.gamejam.papee.entities.Bladder;
+import fr.gamejam.papee.entities.PaPee;
 import org.lwjgl.util.vector.Vector2f;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Game extends GGame {
+
     public static ArrayList<GObject> objects = new ArrayList<GObject>();
 
-    /*
-        Test GUI
-     */
-    GGui gui1 = new GGui(0, "testGUI1", 10, 120, 300, 100);
-    GGui gui2 = new GGui(0, "testGUI2", 10, 10, 300, 100);
-
-
-    /*
-        Test button and particle
-     */
-    GButton button = new GButton("Create Particle", 800, 400) {
-        @Override
-        public void onClick() {
-            for (int i = 0; i < 10; i++) {
-                GParticle p = new GParticle(new Random().nextInt(5), x + sizeX / 2, y + sizeY / 2, new Random().nextInt(32));
-                p.setColor(GDefines.WHITE);
-                p.setSpeed(new Random().nextFloat());
-                p.setDirection(new Vector2f((float) new Random().nextGaussian() * 2, (float) new Random().nextGaussian() * 2));
-                p.setLifetime(new Random().nextFloat() * 2);
-            }
-        }
-    };
+    PaPee papee = new PaPee(0, 0);
+    Bladder bladder = new Bladder(10, 100);
 
     public Game() {
 
