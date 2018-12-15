fd494e74e1e23f19f13f9b75b6e83e26bd3010ce
Maxime Coupas <maxime.coupas@avanade.com>
2018-12-15 14:22:14 +0100

    Implement EnvironmentObject

--- src/main/java/fr/gamejam/papee/entities/environment/EnvironmentObject.java
+++ src/main/java/fr/gamejam/papee/entities/environment/EnvironmentObject.java
@@ -1,5 +1,39 @@
 package fr.gamejam.papee.entities.environment;
 
+import fr.gamejam.papee.entities.environment.items.ItemType;
+
 public abstract class EnvironmentObject {
 
+    private float effectTime;
+    private ItemType itemType;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ItemType getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(ItemType itemType) {
+        this.itemType = itemType;
+    }
+
+    public float getEffectTime() {
+        return effectTime;
+    }
+
+    public void setEffectTime(float effectTime) {
+        this.effectTime = effectTime;
+    }
+
+    public EnvironmentObject(String name, ItemType type) {
+        this.name = name;
+        this.itemType = type;
+    }
 }
