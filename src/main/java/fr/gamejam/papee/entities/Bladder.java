71edda43bd27c85cb44fb367b6ab551664faed13
François POULY <francois.pouly@etu.uca.fr>
2018-12-15 14:20:57 +0100

    Added setters and getters on Bladder.java

--- src/main/java/fr/gamejam/papee/entities/Bladder.java
+++ src/main/java/fr/gamejam/papee/entities/Bladder.java
@@ -1,4 +1,24 @@
 package fr.gamejam.papee.entities;
 
 public class Bladder {
+    private float peeStartLevel;
+    private float peeMaxLevel;
+
+    public float getPeeStartLevel() {
+        return peeStartLevel;
+    }
+
+    public void setPeeStartLevel(float peeStartLevel) {
+        if(peeStartLevel < 100 && peeStartLevel >= 0)
+        this.peeStartLevel = peeStartLevel;
+    }
+
+    public float getPeeMaxLevel() {
+        return peeMaxLevel;
+    }
+
+    public void setPeeMaxLevel(float peeMaxLevel) {
+        if(peeMaxLevel <= 100 && peeMaxLevel > 0)
+            this.peeMaxLevel = peeMaxLevel;
+    }
 }
