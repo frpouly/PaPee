895bfcd9889971342747537ed4d0c73a1ac84ef9
Maxime Coupas <maxime.coupas@avanade.com>
2018-12-15 15:39:20 +0100

    Application of textures

--- src/main/java/fr/gamejam/papee/engine/Game.java
+++ src/main/java/fr/gamejam/papee/engine/Game.java
@@ -3,6 +3,7 @@ package fr.gamejam.papee.engine;
 import fr.gamejam.papee.engine.buttons.GButton;
 import fr.gamejam.papee.engine.fx.GParticle;
 import fr.gamejam.papee.engine.game.GGame;
+import fr.gamejam.papee.engine.graphics.GTexture;
 import fr.gamejam.papee.engine.guis.GGui;
 import fr.gamejam.papee.engine.objects.GObject;
 import fr.gamejam.papee.engine.utils.GDefines;
@@ -31,11 +32,12 @@ public class Game extends GGame {
         @Override
         public void onClick() {
             for (int i = 0; i < 10; i++) {
-                GParticle p = new GParticle(new Random().nextInt(5), x + sizeX / 2, y + sizeY / 2, new Random().nextInt(32));
+                GParticle p = new GParticle(new Random().nextInt(5), x + sizeX / 2, y + sizeY / 2, 32);
                 p.setColor(GDefines.WHITE);
                 p.setSpeed(new Random().nextFloat());
                 p.setDirection(new Vector2f((float) new Random().nextGaussian() * 2, (float) new Random().nextGaussian() * 2));
                 p.setLifetime(new Random().nextFloat() * 2);
+                p.setTexture(GTexture.VIAGRA_TEXTURE);
             }
         }
     };
--- src/main/java/fr/gamejam/papee/entities/environment/items/ItemViagra.java
+++ src/main/java/fr/gamejam/papee/entities/environment/items/ItemViagra.java
@@ -1,11 +1,13 @@
 package fr.gamejam.papee.entities.environment.items;
 
+import fr.gamejam.papee.engine.graphics.GTexture;
 import fr.gamejam.papee.entities.PaPee;
 
 public class ItemViagra extends Item{
 
     public ItemViagra(int ID, float x, float y) {
         super(ID, x, y, ItemType.BONUS, "viagra");
+        setTexture(GTexture.VIAGRA_TEXTURE);
     }
 
     @Override
