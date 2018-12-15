556eb74b8e6ee338a08e74619ebceed6de493e73
François POULY <francois.pouly@etu.uca.fr>
2018-12-15 19:24:41 +0100

    It's working on my machine

--- src/main/java/fr/gamejam/papee/engine/Game.java
+++ src/main/java/fr/gamejam/papee/engine/Game.java
@@ -23,25 +23,29 @@ public class Game extends GGame {
     /*
         Test GUI
      */
-    Item itemViagra = new ItemViagra(1,500, 500);
+    Item itemViagra;
 
-    PaPee papee = new PaPee(0, 0, new Bladder(10, 100));
-    /*
-        Test button and particle
-     */
-    GButton button = new GButton("Create Particle", 800, 400, 16) {
-        @Override
-        public void onClick() {
-            for (int i = 0; i < 10; i++) {
-                GParticle p = new GParticle(new Random().nextInt(5), x + sizeX / 2, y + sizeY / 2, 32);
-                p.setColor(GDefines.WHITE);
-                p.setSpeed(new Random().nextFloat());
-                p.setDirection(new Vector2f((float) new Random().nextGaussian() * 2, (float) new Random().nextGaussian() * 2));
-                p.setLifetime(new Random().nextFloat() * 2);
-                p.setTexture(GTexture.VIAGRA_TEXTURE);
+    private PaPee papee;
+
+    public Game()
+    {
+        objects.clear();
+        itemViagra = new ItemViagra(1,500, 500);
+        papee = new PaPee(0, 0, new Bladder(10, 100));
+        new GButton("Create Particle", 800, 400, 16) {
+            @Override
+            public void onClick() {
+                for (int i = 0; i < 10; i++) {
+                    GParticle p = new GParticle(new Random().nextInt(5), x + sizeX / 2, y + sizeY / 2, 32);
+                    p.setColor(GDefines.WHITE);
+                    p.setSpeed(new Random().nextFloat());
+                    p.setDirection(new Vector2f((float) new Random().nextGaussian() * 2, (float) new Random().nextGaussian() * 2));
+                    p.setLifetime(new Random().nextFloat() * 2);
+                    p.setTexture(GTexture.VIAGRA_TEXTURE);
+                }
             }
-        }
-    };
+        };
+    }
 
     public void update() {
         super.update();
@@ -49,8 +53,7 @@ public class Game extends GGame {
         // Check if Bladder is full
         if(papee.getBladder().isFull())
         {
-            // GAME OVER
+            GGame.window.start(new GameOver());
         }
     }
-
 }
