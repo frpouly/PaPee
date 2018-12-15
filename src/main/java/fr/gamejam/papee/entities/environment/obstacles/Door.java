bb725c9dfde5c99b907e90016d6a75b23795f40d
Louis Dutel <ldutel@itnetwork.fr>
2018-12-15 15:47:00 +0100

    Start implement obstacle

--- src/main/java/fr/gamejam/papee/entities/environment/obstacles/Door.java
+++ src/main/java/fr/gamejam/papee/entities/environment/obstacles/Door.java
@@ -1,4 +1,14 @@
 package fr.gamejam.papee.entities.environment.obstacles;
 
-public class Door {
+import fr.gamejam.papee.entities.PaPee;
+import fr.gamejam.papee.entities.environment.items.ItemType;
+
+public class Door extends Obstacle {
+
+    public Door(int ID, float x, float y) { super(ID, x, y, ItemType.MALUS, "Door"); }
+    
+    @Override
+    public void effect(PaPee papee) {
+
+    }
 }
--- src/main/java/fr/gamejam/papee/entities/environment/obstacles/PeePuddle.java
+++ src/main/java/fr/gamejam/papee/entities/environment/obstacles/PeePuddle.java
@@ -1,4 +1,17 @@
 package fr.gamejam.papee.entities.environment.obstacles;
 
-public class PeePuddle {
+import fr.gamejam.papee.entities.PaPee;
+import fr.gamejam.papee.entities.environment.items.ItemType;
+
+public class PeePuddle extends Obstacle {
+
+    public PeePuddle(int ID, float x, float y) {
+        super(ID, x, y, ItemType.MALUS, "PeePuddle");
+    }
+
+
+    @Override
+    public void effect(PaPee papee) {
+
+    }
 }
