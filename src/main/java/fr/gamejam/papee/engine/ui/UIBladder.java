019e97335aae3bcc6367118ebb92e75e2e0a1c16
François POULY <francois.pouly@etu.uca.fr>
2018-12-16 03:10:17 +0100

    Fixing UIBlader

--- src/main/java/fr/gamejam/papee/engine/ui/UIBladder.java
+++ src/main/java/fr/gamejam/papee/engine/ui/UIBladder.java
@@ -1,13 +1,8 @@
 package fr.gamejam.papee.engine.ui;
 
-import fr.gamejam.papee.engine.Game;
 import fr.gamejam.papee.engine.animation.GAnimation;
 import fr.gamejam.papee.engine.graphics.GGraphics;
-<<<<<<< HEAD
-import fr.gamejam.papee.engine.utils.GDefines;
-=======
 import fr.gamejam.papee.engine.graphics.GTexture;
->>>>>>> 5b6446942e476c7c6a1ed34245ced0351cd33835
 import fr.gamejam.papee.entities.Bladder;
 import fr.gamejam.papee.entities.PaPee;
 
@@ -32,17 +27,10 @@ public class UIBladder implements UI {
 
     @Override
     public void render() {
-<<<<<<< HEAD
-        GGraphics.renderQuad(bladder.getX(), bladder.getY(), bladder.getSizeX(), bladder.getSizeY(), new float[] { 1f, 1f, 1f, 0.8f });
-        GGraphics.renderQuad(bladder.getX(), bladder.getY() + (bladder.getPeeMaxLevel() - bladder.getPeeLevel()) * bladder.getScaleY(), bladder.getSizeX(), bladder.getPeeLevel() * bladder.getScaleY(), new float[] { 1f, 1f, 0f, 0.8f });
 
-        GGraphics.renderImage(animation.current(), bladder.getX(), bladder.getY() + 128, 48, 48, GDefines.WHITE);
-    }
-=======
         GGraphics.renderQuad(bladder.getX() + 18, bladder.getY() + (bladder.getPeeMaxLevel() - bladder.getPeeLevel()) * bladder.getScaleY() + 18, bladder.getSizeX() - 36, bladder.getPeeLevel() * bladder.getScaleY(), new float[] { 1f, 1f, 0f, 0.8f });
         GGraphics.renderOffsetImage(GTexture.GUI_BLADDER, bladder.getX(), bladder.getY(), bladder.getSizeX(), bladder.getSizeY(), new float[] { 1f, 1f, 1f, 1f }, 0, 15, 20, 58);
 
         //GGraphics.renderQuad(bladder.getX(), bladder.getY(), bladder.getSizeX(), bladder.getSizeY(), new float[] { 1f, 1f, 1f, 0.8f });
-            }
->>>>>>> 5b6446942e476c7c6a1ed34245ced0351cd33835
+    }
 }
