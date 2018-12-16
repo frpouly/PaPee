eaf3f7244e946eade0893d0e75a8343b10564d33
François POULY <francois.pouly@etu.uca.fr>
2018-12-16 01:49:39 +0100

    Added the toilets

--- src/main/java/fr/gamejam/papee/engine/Game.java
+++ src/main/java/fr/gamejam/papee/engine/Game.java
@@ -36,11 +36,13 @@ public class Game extends GGame {
 
         LevelLoader l = new LevelLoader("/levels/level1.json");
         itemViagra = new ItemViagra(1,600, 200);
+
         PeePuddle peePuddle = new PeePuddle(4, 300, 300);
         PeePuddle peePuddle2 = new PeePuddle(4, 300, 400);
         PeePuddle peePuddle3 = new PeePuddle(4, 300, 500);
         PeePuddle peePuddle4 = new PeePuddle(4, 300, 600);
-        Toilets toilets = new Toilets(2, 100, 100);
+
+        new Toilets(2, 100, 100);
 
         PaPee papee = new PaPee(200, 200, new Bladder(10, 100));
         map = new Map(papee, l.getTiles());
