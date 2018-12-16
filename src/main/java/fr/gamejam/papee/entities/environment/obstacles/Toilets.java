73843170027ec4d51a79f841c5abdc84a5090b44
François POULY <francois.pouly@etu.uca.fr>
2018-12-16 02:24:54 +0100

    Changed toilets

--- src/main/java/fr/gamejam/papee/entities/environment/obstacles/Toilets.java
+++ src/main/java/fr/gamejam/papee/entities/environment/obstacles/Toilets.java
@@ -2,13 +2,12 @@ package fr.gamejam.papee.entities.environment.obstacles;
 
 import fr.gamejam.papee.engine.graphics.GTexture;
 import fr.gamejam.papee.entities.PaPee;
-import fr.gamejam.papee.entities.environment.items.ItemType;
 
 public class Toilets extends Obstacle {
 
     public Toilets(int ID, float x, float y) {
-        super(ID, x, y, ItemType.BONUS, "viagra");
-        setTexture(GTexture.VIAGRA_TEXTURE);
+        super(ID, x, y, null, "toilets");
+        setTexture(GTexture.TOILETS);
     }
 
     @Override
